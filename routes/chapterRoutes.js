8da4b567b4ff879c8895fa39c2df70ba7992125f
Zaidoon Kamil <igdghfgjhfgvffv68@gmail.com>
2025-06-18 19:04:13 +0300

    .

--- routes/chapterRoutes.js
+++ routes/chapterRoutes.js
@@ -8,7 +8,7 @@ const upload = multer();
 router.post('/chapter', upload.none(), async (req, res) => {
   try {
     const { videoUrl, attachment, summary, lectureId } = req.body;
-    const newChapter = await Chapter.create({ title, videoUrl, attachment, summary, lectureId });
+    const newChapter = await Chapter.create({ videoUrl, attachment, summary, lectureId });
     res.status(201).json(newChapter);
   } catch (error) {
     res.status(500).json({ error: 'خطأ أثناء إضافة الفصل', details: error.message });
